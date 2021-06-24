7753bd537df864feeb855491f95222d50e66ecc8
Mike Fierro <mfierro31@yahoo.com>
2021-06-24 16:41:11 -0700

    getting ready to deploy to Heroku

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const { Client } = require("pg");
 
 const db = new Client({
-  connectionString: process.env.DATABASE_URL || "postgresql:///rickllow"
+  connectionString: process.env.DATABASE_URL || "postgresql:///rickllow",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const PORT = +process.env.PORT || 3001;
 
-app.listen(3001, () => {
-  console.log('Started on http://localhost:3001');
+app.listen(PORT, () => {
+  console.log(`Started on http://localhost:${PORT}`);
 });
